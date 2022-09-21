645de6969fb3bdcfad514024994c555e8ec4a6fc
mathurakash <mathurakash700@gmail.com>
2022-09-21 12:14:32 +0530

    migrations

--- dj1/app1/models.py
+++ dj1/app1/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Teacher(models.Model):
-    name = models
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    
